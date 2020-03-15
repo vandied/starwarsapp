985cbb35bb9d6fa6fc1e1342867d36b5eedb9925
vandied <vandiedarling@gmail.com>
2020-03-15 16:12:46 +0300

    init project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header d-flex">
+            <h3>
+                <a href="#">
+                    Star DB
+                </a>
+            </h3>
+            <ul className="d-flex">
+                <li>
+                    <a href="#"> People</a></li>
+                <li><a href="#">Planet</a></li>
+                <li><a href="#">Starship</a></li>
+            </ul>
+        </div>
+    )
+};
+export default Header;
---
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './RandomPlanet.css';
+
+const RandomPlanet = () => {
+    return (
+        <div className="randomPlanet">
+            <div className="planetImg">
+              <img/>
+            </div>
+            <div className="planetDescription">
+                <h2 className="planetName">PlanetName</h2>
+                <div>Population <span>324432</span></div>
+                <div>Rotation Period <span>32</span></div>
+                <div>Diameter <span>432</span></div>
+            </div>
+        </div>
+    )
+};
+export default RandomPlanet;
