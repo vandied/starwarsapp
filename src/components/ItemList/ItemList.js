890f281461d9cd98bb3e9682b46f81b3bcd0bc6c
vandied <vandiedarling@gmail.com>
2020-03-15 22:00:32 +0300

    add prettier and fix styles

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 import './ItemList.css';
 
 const ItemList = () => {
-    return (
-        <div className="itemList">
-            <ul className="listGroup">
-                <li className="listGroupItem">Luke Skywalker</li>
-                <li className="listGroupItem">Darth Veider</li>
-                <li className="listGroupItem">R2-D2</li>
-            </ul>
-        </div>
-    )
+	return (
+		<div className="itemList">
+			<ul className="listGroup">
+				<li className="listGroupItem">Luke Skywalker</li>
+				<li className="listGroupItem">Darth Veider</li>
+				<li className="listGroupItem">R2-D2</li>
+			</ul>
+		</div>
+	);
 };
 export default ItemList;
