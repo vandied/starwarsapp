acd7229a3dbfe2017d6dff15424fbd7d620505b9
vandied <vandiedarling@gmail.com>
2020-05-01 08:16:34 +0300

    add hoc composed

--- src/components/moviesComponents/itemLists.js
+++ src/components/moviesComponents/itemLists.js
@@ -1,11 +1,31 @@
 import ItemList from "../ItemList";
 import { withData } from "../HOCHelpers";
 import MovieService from "../../services/movie-service";
-
+import React from "react";
 const movieService = new MovieService();
 const { getPopularMovies, getPopularPeople, getPopularTV } = movieService;
-const PersonList = withData(ItemList, getPopularPeople);
-const MovieList = withData(ItemList, getPopularMovies);
-const TVList = withData(ItemList, getPopularTV);
+
+const withChildFunction = (Wrapped, fn) => {
+  return props => {
+    return <Wrapped {...props}>{fn}</Wrapped>;
+  };
+};
+
+const renderName = ({ name }) => <span>{name}</span>;
+const renderMovieTitle = ({ title, popularity }) => (
+  <span>
+    {title} ({popularity})
+  </span>
+);
+
+const PersonList = withData(
+  withChildFunction(ItemList, renderName),
+  getPopularPeople
+);
+const MovieList = withData(
+  withChildFunction(ItemList, renderMovieTitle),
+  getPopularMovies
+);
+const TVList = withData(withChildFunction(ItemList, renderName), getPopularTV);
 
 export { PersonList, MovieList, TVList };
